f96e9e30b3ad02c3ef4bf2db79913a66952d4142
M0hamady <119875626+M0hamady@users.noreply.github.com>
2023-12-07 02:06:26 +0200

    vercel update

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 # build_files.sh
-which python3.9
+which python3.10
 pwd
 cd /usr/local/bin/
 yum install sqlite-devel -y
@@ -7,4 +7,4 @@ yum install sqlite-devel -y
 make && make altinstall
 cd /vercel/path0/
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+python3.10 manage.py collectstatic --noinput
