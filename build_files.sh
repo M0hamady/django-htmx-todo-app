ea6fdae364d43a6674fb6627ab5a4d4d16d1687d
M0hamady <119875626+M0hamady@users.noreply.github.com>
2023-12-07 02:54:26 +0200

    vercel update

--- build_files.sh
+++ build_files.sh
@@ -1,20 +1,11 @@
-#!/bin/bash
-
-set -e  # Exit immediately if any command fails
-
-# Install SQLite development libraries
-if [[ -f /etc/redhat-release ]]; then
-    # Red Hat or CentOS-based system
-    sudo yum install sqlite-devel -y
-elif [[ -f /etc/lsb-release ]]; then
-    # Ubuntu or Debian-based system
-    sudo apt-get install libsqlite3-dev -y
-fi
-
+# build_files.sh
+which python3.9
+pwd
+cd /usr/local/bin/
+yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make && make altinstall
 cd /vercel/path0/
-
-# Install dependencies
-python3.9 -m pip install -r requirements.txt
-
-# Collect static files
+python3.9 -m pip install --upgrade pip
+pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
