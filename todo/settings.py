9196ac48705b49a5b8d9c60ab56a96d863549767
M0hamady <119875626+M0hamady@users.noreply.github.com>
2023-12-07 02:24:46 +0200

    vercel update

--- todo/settings.py
+++ todo/settings.py
@@ -76,12 +76,15 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'y8YvdCH9BwnV',
+        'HOST': 'ep-lingering-cloud-93038963.us-east-1.postgres.vercel-storage.com',
+        'PORT': '5432',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
