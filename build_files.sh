1f85de1b8db18e5b79e1a6edd7d17437d305d967
M0hamady <119875626+M0hamady@users.noreply.github.com>
2023-12-07 02:49:34 +0200

    vercel update

--- build_files.sh
+++ build_files.sh
@@ -6,6 +6,6 @@ yum install sqlite-devel -y
 ./configure --enable-optimizations --enable-loadable-sqlite-extensions
 make && make altinstall
 cd /vercel/path0/
-python.exe -m pip install --upgrade pip
+python3.9 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3.9 manage.py collectstatic --noinput
